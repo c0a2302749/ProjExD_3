496f0273de5ca771f31c1a3f6a24472e47c13c23
C0A23027 <c0a2302749@edu.teu.ac.jp>
2024-10-08 14:59:57 +0900

    ゲームオーバー画面

--- fight_kokaton.py
+++ fight_kokaton.py
@@ -168,6 +168,8 @@ def main():
         if beam and bomb:
             if beam.rct.colliderect(bomb.rct):
                 bomb,beam = None,None
+                bird.change_img(6, screen)
+                pg.display.update()
                 
 
         key_lst = pg.key.get_pressed()
